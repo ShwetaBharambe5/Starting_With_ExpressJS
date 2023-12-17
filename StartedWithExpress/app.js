6f6f6ba259a33690d43ea568b1bb89ddd6b7e15d
Shweta Mohan Bharambe <shwetabharambe21@gmail.com>
2023-12-17 18:26:32 +0530

    third commit

--- StartedWithExpress/app.js
+++ StartedWithExpress/app.js
@@ -4,20 +4,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require('./routes/admin');
 
-app.use('/add-product', (req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
-});
+const shopRoutes =  require('./routes/shop');
 
-app.post('/product', (req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/', (req, res, next)=>{
-    res.send("<h1>Hello from Express</h1>");
-    //res.send({ key1: 10 });
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>page not found</h1>');
 });
 
 app.listen(5000);
