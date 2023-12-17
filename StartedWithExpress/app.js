f670aca7654a01c374250fdcf5ed38ea8c780dd1
Shweta Mohan Bharambe <shwetabharambe21@gmail.com>
2023-12-17 17:19:47 +0530

    second commit

--- StartedWithExpress/app.js
+++ StartedWithExpress/app.js
@@ -1,15 +1,21 @@
 const express =  require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("Inside the Middleware");
-    next();
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product', (req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next)=>{
-    console.log("Inside another middleware");
+app.post('/product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
 
+app.use('/', (req, res, next)=>{
     res.send("<h1>Hello from Express</h1>");
     //res.send({ key1: 10 });
 });
