f661eaf745313b26056ad9cbc78c56963a5d886c
Shweta Mohan Bharambe <shwetabharambe21@gmail.com>
2023-12-20 22:12:51 +0530

    seventh commit

--- Book_a_Call_Project/controllers/error.js
+++ Book_a_Call_Project/controllers/error.js
@@ -1,7 +1,11 @@
-const path = require('path');
+// const path = require('path');
 
-const rootdir = require('../util/path');
+// const rootdir = require('../util/path');
 
-exports.get404 = (req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// exports.get404 = (req, res, next)=>{
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// };
+
+exports.get404 = (req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 };
