4750281acadcae9867b1f9ca964271e8e7f33b90
Shweta Mohan Bharambe <shwetabharambe21@gmail.com>
2023-12-24 00:05:15 +0530

    sixteenth commit

--- Assignment_8/util/database.js
+++ Assignment_8/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: 'root'            //Its password we gave while installation
 });
 
 module.exports = pool.promise();
